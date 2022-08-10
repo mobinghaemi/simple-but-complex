df450ebc647ef19ae7faf4c7c59fad3c6e9a0626
mobin-ghaemi <mobinghaemi@yahoo.com>
2022-08-10 10:30:46 +0430

    UniqueInOrder Challenge

---
+++ 08-uniqueInOrder/script.js
@@ -0,0 +1,10 @@
+const unique = iterable => {
+    
+    for (let i = 0; i < iterable.length; i++) {
+        iterable[i] === iterable[i + 1] && iterable.splice(i, 1)
+    }
+
+    return iterable
+}
+
+console.log(unique([1, 1, 3, 4, 4, 5, 5]));
