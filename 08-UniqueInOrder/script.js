9078626090c8efa2928bca61998cee6890940dd7
mobin-ghaemi <mobinghaemi@yahoo.com>
2022-08-10 10:42:28 +0430

    Unique : Method 2 (ES6)

--- 08-UniqueInOrder/script.js
+++ 08-UniqueInOrder/script.js
@@ -1,7 +1,8 @@
 const unique = iterable => {
-    for (let i = 0; i < iterable.length; i++)
-        iterable[i] === iterable[i + 1] && iterable.splice(i, 1)
-    return iterable
+    // for (let i = 0; i < iterable.length; i++)
+    //     iterable[i] === iterable[i + 1] && iterable.splice(i, 1)
+    // return iterable
+    return new Set(iterable)
 }
 
 console.log(unique([1, 1, 3, 4, 4, 5, 5]));
